f64ac195a03d3554dd5222340fa45cd14966bd5a
David Enete <david.enete@scientificgames.com>
2016-05-06 10:15:43 -0400

    [denete] update with "cm" references removed

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ A jQuery plugin to allow text elements on a page to be constrained by size (curr
 
 ## Usage:
 
->        cmg.query('.cmTextElement').cmtextconstrain({
->                event: 'click', // for future extensibility to allow other events
->                onExpose: function(){}, // a function to be run once the element is exposed
->                onConstrain: function(){}, // a function to be run once the element is constrained
->                restrict: {type: 'words', limit: 6}, // type can be 'words' or 'chars'
->                showControl: {string: '[ + ]', title: 'Show More', addclass: 'cmShowHide'}, // element shown to expose
->                hideControl: {string: '[ - ]', title: 'Show Less', addclass: 'cmShowHide'}, // element shown to constrain
->                trailingString: '...' // string to show at end of truncated text
->                });
+>        $('.text-element').textconstrain({
+>            event: 'click', // for future extensibility to allow other events
+>            onExpose: function(){}, // a function to be run once the element is exposed
+>            onConstrain: function(){}, // a function to be run once the element is constrained
+>            restrict: {type: 'words', limit: 6}, // type can be 'words' or 'chars'
+>            showControl: {string: '[ + ]', title: 'Show More', addclass: 'show-hide'}, // element shown to expose
+>            hideControl: {string: '[ - ]', title: 'Show Less', addclass: 'show-hide'}, // element shown to constrain
+>            trailingString: '...' // string to show at end of truncated text
+>        });
 
 ## Methods:
 
->        cmg.query('.cmTextElement').cmtextconstrain('destroy'); // destroy the instance of cmtextconstrain
+>        $('.text-element').textconstrain('destroy'); // destroy the instance of textconstrain
--- js/jquery/jquery.textconstrain.js
+++ js/jquery/jquery.textconstrain.js
@@ -1,23 +1,23 @@
 /**
- * Plugin: jQuery Text-constrain, jquery.cmtextconstrain.js
- * Copyright: Copyright (c) 2011 CMGdigital
- * Version: 1.0.0
+ * Plugin: jQuery Text-constrain, jquery.textconstrain.js
+ * Copyright: Copyright (c) 2016 David Enete
+ * Version: 1.0.1
  * Author: David A. Enete
- * Date: 24 February 2011
+ * Date: 6 May 2016
  * Description: jQuery Text-constrain plugin - A jQuery plugin to allow text elements on a page to be constrained by size.
  */
 
 (function($) {
-    
+
     // public methods
     var methods = {
         init: function(options) {
             return this.each(function() {
                 var $this = $(this);
-                data = $this.data('cmtextconstrain');
+                data = $this.data('textconstrain');
                 if(!data){
-                    opts = $.extend({}, $.fn.cmtextconstrain.defaults, options);
-                    $this.data('cmtextconstrain', opts);
+                    opts = $.extend({}, $.fn.textconstrain.defaults, options);
+                    $this.data('textconstrain', opts);
                     if(!($this.attr('id'))){
                         var dateObj = new Date();
                         var dateString = String(dateObj.getTime());
@@ -27,19 +27,19 @@
                     $this.clone().insertBefore($this).attr({id: cloneID});
                     $this.hide();
                     var $elemClone = $('#' + cloneID);
-                    
+
                     // create constrained string
                     if(opts.restrict['type'] == 'words'){
                         var wordArr = $elemClone.text().split(/\s+/);
                         if(wordArr.length <= opts.restrict['limit']){
-                            $this.cmtextconstrain('destroy');
+                            $this.textconstrain('destroy');
                             return;
                         } else {
                             var shortString = wordArr.slice(0,opts.restrict['limit']).join(' ');
                         }
                     } else if(opts.restrict['type'] == 'chars'){
                         if($this.text().length <= opts.restrict['limit']){
-                            $this.cmtextconstrain('destroy');
+                            $this.textconstrain('destroy');
                             return;
                         } else {
                             var charPointer = opts.restrict['limit'];
@@ -52,17 +52,17 @@
                         }
                     }
                     shortString += opts.trailingString;
-                    
+
                     $elemClone.text(shortString);
-                    $elemClone.append('&nbsp;<a href="javascript:void(0);" class="cmExpose ' + opts.showControl['addclass'] + '" title="' + opts.showControl['title'] + '">' + opts.showControl['string'] + '</a>');
-                    $this.append('&nbsp;<a href="javascript:void(0);" class="cmConstrain ' + opts.hideControl['addclass'] + '" title="' + opts.hideControl['title'] + '">' + opts.hideControl['string'] + '</a>');
-                    
+                    $elemClone.append('&nbsp;<a href="javascript:void(0);" class="expose ' + opts.showControl['addclass'] + '" title="' + opts.showControl['title'] + '">' + opts.showControl['string'] + '</a>');
+                    $this.append('&nbsp;<a href="javascript:void(0);" class="constrain ' + opts.hideControl['addclass'] + '" title="' + opts.hideControl['title'] + '">' + opts.hideControl['string'] + '</a>');
+
                     // need to implement delay if event is hover / mouseover
-                    $elemClone.find('.cmExpose').bind(opts.event, function(){
+                    $elemClone.find('.expose').bind(opts.event, function(){
                         _expose($this,$elemClone);
                         opts.onExpose.call(this);
                     });
-                    $this.find('.cmConstrain').bind(opts.event, function(){
+                    $this.find('.constrain').bind(opts.event, function(){
                         _expose($elemClone,$this);
                         opts.onConstrain.call(this);
                     });
@@ -72,10 +72,10 @@
         destroy: function(){
             return this.each(function(){
                 var $this = $(this);
-                data = $this.data('cmtextconstrain');
+                data = $this.data('textconstrain');
                 if(data){
                     $('#' + $this.attr('id') + '_clone').remove();
-                    $this.show().removeData('cmtextconstrain');
+                    $this.show().removeData('textconstrain');
                 }
             })
         }
@@ -88,25 +88,25 @@
     }
 
     // passing public method calls
-    $.fn.cmtextconstrain = function(method){
+    $.fn.textconstrain = function(method){
         if(methods[method]){
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof method === 'object' || ! method){
             return methods.init.apply(this, arguments);
         } else {
-            console.log('Method ' +  method + ' does not exist on jQuery.cmtextconstrain');
+            console.log('Method ' +  method + ' does not exist on jQuery.textconstrain');
         }
     };
 
     // setting defaults for the plugin
-    $.fn.cmtextconstrain.defaults = {
+    $.fn.textconstrain.defaults = {
         event: 'click',
         onExpose: function(){},
         onConstrain: function(){},
         restrict: {type: 'chars', limit: 121}, // ['chars', 'words']
-        showControl: {string: '[&nbsp;+&nbsp;]', title: 'Show More', addclass: 'cmShowHide'},
-        hideControl: {string: '[&nbsp;-&nbsp;]', title: 'Show Less', addclass: 'cmShowHide'},
+        showControl: {string: '[&nbsp;+&nbsp;]', title: 'Show More', addclass: 'show-hide'},
+        hideControl: {string: '[&nbsp;-&nbsp;]', title: 'Show Less', addclass: 'show-hide'},
         trailingString: '...'
     };
 
-})(cmg.query);
+})(jQuery);
